940f37b3847785c65c9ff57ecd4fef4f0227b49b
ndpdarshana <ndp.mymail@gmail.com>
2020-04-06 21:57:16 +1000

    13: Hitting the API

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === 'OPTIONS'){
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -23,7 +33,7 @@ app.use(
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-jkddc.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
   .then(() =>{
-    app.listen(3000);
+    app.listen(8000);
   }).catch(err => {
     console.log(err);
   });
